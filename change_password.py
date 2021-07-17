b769b723252fdbe40977210009d2bf1da325822c
joseph_hsieh <joseph_hsieh@trendmicro.com>
2021-07-17 14:47:02 +0800

    enhance test script

--- change_password.py
+++ change_password.py
@@ -6,6 +6,9 @@ def change_password(old, new):
     response = requests.post(
         'http://127.0.0.1:5000/change_password',
         data={'old_password': old, 'new_password': new})
+    print("Old password:", old)
+    print("New password:", new)
+    print("Result:")
     print(response.status_code)
     print(response.reason)
     print(response.text)
